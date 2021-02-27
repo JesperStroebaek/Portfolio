09ac160840aa16a0fa157cc5c233576a57e31974
Jesper Stroebaek <hamdererindebatman@protonmail.com>
2021-02-28 00:22:30 +0100

    2 push

--- src/main/java/com/example/portefolio/Control/PortController.java
+++ src/main/java/com/example/portefolio/Control/PortController.java
@@ -4,25 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.swing.text.html.HTML;
+
 @Controller
 public class PortController {
     @GetMapping("/")
-    @ResponseBody
-    public void portfolio(){ }
-    @GetMapping("portfolio1/edu")
-    @ResponseBody
-    public void portfolioJobs(){}
-    @GetMapping("portfolio1/job")
-    @ResponseBody
-    public void portfolioGit(){}
-    @GetMapping("/git")
-    @ResponseBody
-    public void portfolioInfo(){}
-    @GetMapping("/info")
-    @ResponseBody
-    public void portfolioEdu(){}
+
+
+
+    public String portfolio1() throws Exception
+
+    {
+        return portfolio1();
+    }
+
+    }
 
 
 
 
-}
+
+
--- src/main/java/com/example/portefolio/Control/PortefolioApplication.java
+++ src/main/java/com/example/portefolio/Control/PortefolioApplication.java
@@ -1,4 +1,4 @@
-package com.example.portefolio;
+package com.example.portefolio.Control;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
